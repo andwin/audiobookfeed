c652c51802b629c224ffb7e4318b13d589723265
Felix Eckhofer <felix@eckhofer.com>
2013-06-18 14:47:51 +0200

    support https

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ if ($handle = opendir('mp3')) {
                 if (strpos($file, '.') !== 0) {
                         $audiobook = array();
                         $audiobook[ 'name' ] = str_replace( '.mp3', '', $file);
-                        $audiobook[ 'url' ] = 'http://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT']. '/mp3/' . $file;
+                        $audiobook[ 'url' ] = $_SERVER['HTTPS']?'https':'http' . '://' .
+                                $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT']. '/mp3/' . $file;
                         $audiobook[ 'size' ] = filesize( 'mp3/' . $file );
 
                         $audiobooks[] = $audiobook;
